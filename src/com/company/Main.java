b2aa596dae6cb2d8a332093c21821466f15c84c2
SwordInStone <MilkCup@yandex.ru>
2021-08-22 09:53:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+import java.util.Date;
+public class Main {
+
+
+        static char massive[][] = new char[5][5];
+
+        public static void main(String[] args) throws Exception {
+            fill(massive);
+
+        }
+
+        private static char[][] fill(char massive[][]){
+            for(int x=0;x<5;x++){
+                for(int y=0;y<5;y++){
+                    if(random(x,y))massive[x][y]='.';
+                    else massive[x][y]='#';
+                    System.out.print(massive[x][y]+" ");
+                }
+                System.out.println("");
+
+            }
+            return massive;
+        }
+        private static boolean random(int x,int y){
+            long random=25*(new Date().getTime()%4);
+            try {
+                Thread.sleep(1);
+            }
+            catch(Exception e){
+            }
+            while(y>0){
+                if(massive[x][y]=='#')
+                    random+=33;
+                else if(massive[x][y]=='.')random-=66;
+                y--;
+            }
+            boolean result=true;
+            if(random<75)return false;
+
+            return result;
+        }
+
+}
