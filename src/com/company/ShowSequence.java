7a8b0240f42dcbac29ee615e4349981111f97e82
Petr Sha <MilkCup@yandex.ru>
2021-08-23 21:51:24 +0300

    Bag odd number was fixed. v1

--- src/com/company/ShowSequence.java
+++ src/com/company/ShowSequence.java
@@ -3,12 +3,17 @@ package com.company;
 public class ShowSequence {
 
     public static void main(String args[]){
-        System.out.println(showSequence(6));
+        System.out.println(showSequence(7));
     }
     public static String showSequence(int value){
         Integer result=0;
+        if(value%2==1){
+            result+=value;
+            value-=1;
+        }
         int first=0;
         int second=value;
+
         while(first!=second){
             result+=first+second;
             first++;second--;
