52b65afbb978869d21041c651cebabcb7bc30ceb
Petr Sha <MilkCup@yandex.ru>
2021-09-11 10:54:36 +0300

    test commit for next merge

--- src/com/company/ShowSequence.java
+++ src/com/company/ShowSequence.java
@@ -6,7 +6,7 @@ public class ShowSequence {
         System.out.println(showSequence(7));
     }
     public static String showSequence(int value){
-        if(value<0)return "value < 0";
+        /*if(value<0)return "value < 0";
         Integer result=0;
         if(value%2==1){
             result+=value;
@@ -15,7 +15,19 @@ public class ShowSequence {
         int HalfNumber=value/2;
         result+=HalfNumber*value;
         result+=HalfNumber;
-        return result.toString();
+        return result.toString();*/
+
+        String result="";
+        if(value<0)return value+"<0";
+        if(value==0)return "0=0";
+        int i;
+        for(i=0;i<value;i++){
+            result+=i+"+";
+        }
+        result+=i+"="+(value+1)*value/2;
+        return result;
+
+    }
     }
 
-}
+
